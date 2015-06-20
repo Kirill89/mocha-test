4ab661425b745a851c2d2f30b4ae52ed286f2a13
Kirill Efimov <kirill89@gmail.com>
2015-06-20 18:23:58 +0700

    first commit

---
+++ test/test.js
@@ -0,0 +1,12 @@
+/*global describe, it, assert, before, after*/
+'use strict';
+
+describe('blabla test', function () {
+  before(function(done) {
+    this.skip();
+    done();
+  });
+  it('blabla case', function (done) {
+    done();
+  });
+});
